d8f2d8d6b9befaf1c7f8f12ead459c94cb910abe
Tavis Aitken <tavisto@tavisto.net>
2013-02-01 15:02:32 -0700

    Got lostash up and running.

--- puppet/manifests/indexer.pp
+++ puppet/manifests/indexer.pp
@@ -20,5 +20,23 @@ node indexer {
     jarfile     => '/vagrant/logstash-1.1.9-monolithic.jar',
     provider    => 'custom',
     installpath => '/var/logstash',
+    status      => 'unmanaged',
   }
+
+
+  logstash::input::tcp { 'apache':
+    type => 'apache',
+    port => 3333,
+    tags => ['apache','access']
+  }
+
+  logstash::filter::grok { 'apache-access':
+    type    => 'apache',
+    pattern => ['%{COMBINEDAPACHELOG} (?:%{QS:cookie})',]
+  }
+
+  logstash::output::elasticsearch_http { 'local':
+    host => '127.0.0.1',
+  }
+
 }
